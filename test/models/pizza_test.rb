4e654faa806e09f5c57b633cd6c3ebc3c6da68e9
Benjamin-Couey <benjamincouey36@gmail.com>
2025-02-13 15:29:12 -0700

    Updated topping and pizza fixtures to have more descriptive names

--- test/models/pizza_test.rb
+++ test/models/pizza_test.rb
@@ -3,37 +3,37 @@ require "test_helper"
 class PizzaTest < ActiveSupport::TestCase
 
 	test "should not allow duplicate pizza names" do
-    dupPizza = Pizza.new(name: pizzas(:one).name)
+    dupPizza = Pizza.new(name: pizzas(:special).name)
 		assert_not dupPizza.valid?
 	end
 
 	test "should not allow duplicate pizza topping combos" do
-		dupPizza = Pizza.new(toppings: pizzas(:one).toppings)
+		dupPizza = Pizza.new(toppings: pizzas(:special).toppings)
 		assert_not dupPizza.valid?
 	end
 
 	test "should allow pizza to have duplicates of a topping" do
-		allToppingOnePizza = Pizza.new(toppings: [toppings(:one), toppings(:one), toppings(:one)])
+		allToppingOnePizza = Pizza.new(toppings: [toppings(:sausage), toppings(:sausage), toppings(:sausage)])
 		assert allToppingOnePizza.valid?
 	end
 
 	test "should not prevent updating pizza while keeping same toppings" do
-		pizzas(:one).name = "newName"
-		assert pizzas(:one).valid?
+		pizzas(:special).name = "newName"
+		assert pizzas(:special).valid?
 	end
 
 	test "pizza is vegetarian only if it and all its toppings are vegetarian" do
-		assert_not pizzas(:one).is_vegetarian()
-		assert pizzas(:three).is_vegetarian()
-		pizzas(:three).toppings << toppings(:one)
-		assert_not pizzas(:three).is_vegetarian()
+		assert_not pizzas(:special).is_vegetarian()
+		assert pizzas(:veg_mushroom).is_vegetarian()
+		pizzas(:veg_mushroom).toppings << toppings(:sausage)
+		assert_not pizzas(:veg_mushroom).is_vegetarian()
 	end
 
 	test "numeric_total returns sum of numeric attribute for pizza and all its toppings and nil for non numeric attributes" do
-		assert_equal 4.5, pizzas(:one).numeric_total("price")
-		assert_equal 3, pizzas(:one).numeric_total("calories")
-		assert_nil pizzas(:one).numeric_total("name")
-		assert_nil pizzas(:one).numeric_total("notafield")
+		assert_equal 4.5, pizzas(:special).numeric_total("price")
+		assert_equal 30, pizzas(:special).numeric_total("calories")
+		assert_nil pizzas(:special).numeric_total("name")
+		assert_nil pizzas(:special).numeric_total("notafield")
 	end
 
 end
--- test/models/topping_test.rb
+++ test/models/topping_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ToppingTest < ActiveSupport::TestCase
 
 	test "should not allow duplicate toppings" do
-		dupTopping = Topping.new(name: toppings(:one).name)
+		dupTopping = Topping.new(name: toppings(:sausage).name)
 		assert_not dupTopping.valid?
 	end
 end
