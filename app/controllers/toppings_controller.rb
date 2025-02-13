8b04f7b1aa034b7b9c020d677f3563a25ac97ef1
Benjamin-Couey <benjamincouey36@gmail.com>
2025-02-13 07:04:59 -0700

    First pass at controllers and views for pizzas and toppings

--- app/controllers/toppings_controller.rb
+++ app/controllers/toppings_controller.rb
@@ -1,19 +1,42 @@
 class ToppingsController < ApplicationController
   def index
+    @toppings = Topping.all
   end
 
   def new
+    @topping = Topping.new()
   end
 
   def create
+    @topping = Topping.new(topping_params)
+    if @topping.save
+      redirect_to toppings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @topping = Topping.find(params[:id])
   end
 
   def update
+    @topping = Topping.find(params[:id])
+    if @topping.update(topping_params)
+      redirect_to toppings_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @topping = Topping.find(params[:id])
+    @topping.destroy()
+    redirect_to toppings_path
   end
+
+  private
+    def topping_params
+      params.require(:topping).permit(:name, :vegetarian, :price, :calories)
+    end
 end
