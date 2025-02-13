ed071c31e3ceeb0ece1115e65b306b3ed4313c21
Benjamin-Couey <benjamincouey36@gmail.com>
2025-02-13 12:28:56 -0700

    Fixed error when there's no user session, added flash messages to redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,22 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :current_user
 
+  def user_from_session
+    return User.find_by(id: session[:user_id])
+  end
+
   def current_user
-    @current_user = User.find(session[:user_id])
+    @current_user = user_from_session()
   end
 
   def authorize_store_owner
-    redirect_to :root unless User.find(session[:user_id]).has_role(Role::STORE_OWNER)
+    flash[:error] = "Only store owners may access that page"
+    redirect_to :root unless user_from_session()&.has_role(Role::STORE_OWNER)
   end
 
   def authorize_chef
-    redirect_to :root unless User.find(session[:user_id]).has_role(Role::CHEF)
+    flash[:error] = "Only chefs may access that page"
+    redirect_to :root unless user_from_session()&.has_role(Role::CHEF)
   end
 
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
 
   def select
     session[:user_id] = selected_user_params
-    flash[:notice] = "Now 'logged in' as the user #{User.find(session[:user_id]).name}"
+    flash[:notice] = "Now 'logged in' as the user #{user_from_session()&.name}"
     redirect_to :root
   end
 
